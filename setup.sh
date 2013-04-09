4d2e9b0b41ee0dbe783293ca43f2f29d48c3051e
Sheng Yu <yusheng@google.com>
2013-04-09 15:34:47 -0700

    Add a script to setup the environment

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+set -e
+
+TMP=$(tempfile -p rc-)
+rm -rf "$TMP"
+mkdir "$TMP"
+
+curl https://nodeload.github.com/saintfish/rc/zip/master > $TMP/rc.zip
+unzip $TMP/rc.zip -d $TMP
+cp -f -T -r $TMP/rc-master $HOME/rc
+for file in $HOME/rc/sample.*; do
+  dest_file=${file/rc\/sample/}
+  cp "$file" "$dest_file"
+done
